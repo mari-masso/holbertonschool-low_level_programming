9b1d71bc19fd0339a37301af55ff381c27f53ef6
mari-masso <mariana_massonnier@hotmail.com>
2023-02-18 16:17:09 -0800

    9.4.0

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,16 @@
 #include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Output of a string
+ *
+ * Return: 0(Success)
+ */
+
 int main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle";
+	char str[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str);
 
