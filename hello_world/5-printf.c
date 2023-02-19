3fa939f0eb2846335cb8367e3ba552824077aaed
mari-masso <mariana_massonnier@hotmail.com>
2023-02-19 09:54:04 -0800

    task 9.5.0

---
+++ hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ *  * main - Entry point
+ *   * 
+ *    * Description: 'using printf to print a statement'
+ *     * 
+ *      * Return: Always 0 (Success)
+ *       */
+
+int main(void)
+
+{
+	char statement[];
+	printf("with proper grammar, but the outcome is a piece of art", statement);
+
+	return (0);
+
+}
