4ecaea5475e106f03928efc1ba7a0e8648ef9260
mari-masso <mariana_massonnier@hotmail.com>
2023-02-19 10:19:48 -0800

    task 9.5.0

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,19 +1,17 @@
 #include<stdio.h>
 
 /**
- *  * main - Entry point
- *   * 
- *    * Description: 'using printf to print a statement'
- *     * 
- *      * Return: Always 0 (Success)
- *       */
+ * main - Entry point
+ *
+ * Description: 'Printing a statement with printf'
+ *
+ * Return: 0 (Success)
+ */
 
 int main(void)
 
 {
-	char statement[];
-	printf("with proper grammar, but the outcome is a piece of art", statement);
+	printf("%s \n", "with proper grammar, but the outcome is a piece of art,");
 
 	return (0);
-
 }
