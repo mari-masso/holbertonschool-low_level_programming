0a08f254c47fa2a4ca820f7f4de6e84fd972bbb7
mari-masso <mariana_massonnier@hotmail.com>
2023-02-21 09:48:43 -0800

    task 10.5.0

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: '
+ *
+ * Return: 0(Success)
+ *
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
