142d01296246649c4495a35ac335be9ce0682206
mari-masso <mariana_massonnier@hotmail.com>
2023-02-21 16:29:13 -0800

    task 11.0.0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Printing the
+ * string _putchar'
+ *
+ * Return: 0(Success)
+ *
+ */
+
+int main(void)
+{
+	int ch = 95;
+
+	printf("%cputchar", ch);
+	putchar('\n');
+	return (0);
+}
