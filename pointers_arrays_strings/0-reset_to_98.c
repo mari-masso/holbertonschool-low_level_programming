db788e2b47b91ef07f649e222c9b665f41250d6f
mari-masso <mariana_massonnier@hotmail.com>
2023-02-27 10:26:31 -0800

    task 13.0.0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,12 +2,12 @@
 
 /**
  * reset_to_98 - Entry point
- *
+ * @n: n is the pointer to p variable
  * Description: Write a function that
  * takes a pointer to an int as parameter
- * and updates the value it points to to 98.
+ i* and updates the value it points to to 98.
  *
- * */
+ */
 
 void reset_to_98(int *n)
 {
