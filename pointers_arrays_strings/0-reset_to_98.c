63dff6454f5fd78c632275f519d68a1be610ccf2
mari-masso <mariana_massonnier@hotmail.com>
2023-02-27 10:17:11 -0800

    task 13.0.0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ *
+ * Description: Write a function that
+ * takes a pointer to an int as parameter
+ * and updates the value it points to to 98.
+ *
+ * Return: 0(Success)
+ */
+
+void reset_to_98(int *n)
+{
+	int p;
+
+	p = 98;
+	*n = p;
+}
