4feed31bcd8d899b2e1964c2b179623648dc2730
mari-masso <mariana_massonnier@hotmail.com>
2023-02-28 08:25:57 -0800

    task 13.2.0

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - entry point
+ *
+ * @s: the string to be measured in length
+ * Description: 'Write a function that
+ * returns the length of a string.'
+ *
+ * Return: 0(Success)
+ */
+
+int _strlen(char *s)
+{
+	char a;
+
+	while ((*s = a) != '\0')
+	{
+		a++;
+	}
+	return (a);
+}
